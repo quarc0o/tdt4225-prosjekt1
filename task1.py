2a3cb5d6327d1f9436deea00acd6090116a685d7
quarc0o <emmanuelq150@gmail.com>
2023-09-27 16:38:16 +0200

    working, but slow insert - only user 020

--- task1.py
+++ task1.py
@@ -1,5 +1,7 @@
 from DbConnector import DbConnector
 from tabulate import tabulate
+import os
+    
 
 
 class ExampleProgram:
@@ -11,31 +13,66 @@ class ExampleProgram:
 
     def create_tables(self):
         userTable = """CREATE TABLE IF NOT EXISTS User (
-                   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                   name VARCHAR(30))
-                """
+                    id VARCHAR(5) NOT NULL PRIMARY KEY,
+                    has_labels BOOLEAN NOT NULL)
+                    """
         activityTable = """CREATE TABLE IF NOT EXISTS Activity (
-                   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                   name VARCHAR(30))
-                """
-        
+                    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                    user_id VARCHAR(5) NOT NULL,
+                    transportation_mode VARCHAR(30),
+                    start_date_time DATETIME NOT NULL,
+                    end_date_time DATETIME NOT NULL,
+                    FOREIGN KEY(user_id) REFERENCES User(id))
+                    """
         trackpointTable = """CREATE TABLE IF NOT EXISTS TrackPoint (
-                   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                   name VARCHAR(30))
-                """
-        # This adds table_name to the %s variable and executes the query
+                    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                    activity_id INT NOT NULL,
+                    lat DOUBLE NOT NULL,
+                    lon DOUBLE NOT NULL,
+                    altitude INT NOT NULL,
+                    date_days DOUBLE NOT NULL,
+                    date_time DATETIME NOT NULL,
+                    FOREIGN KEY(activity_id) REFERENCES Activity(id))
+                    """
         self.cursor.execute(userTable)
         self.cursor.execute(activityTable)
         self.cursor.execute(trackpointTable)
         self.db_connection.commit()
 
-    """ def insert_data(self, table_name):
-        names = ['Bobby', 'Mc', 'McSmack', 'Board']
-        for name in names:
-            # Take note that the name is wrapped in '' --> '%s' because it is a string,
-            # while an int would be %s etc
-            query = "INSERT INTO %s (name) VALUES ('%s')"
-            self.cursor.execute(query % (table_name, name))
+    def read_labels(self, user_path):
+        labels_path = os.path.join(user_path, "labels.txt")
+        if not os.path.exists(labels_path):
+            return []
+        with open(labels_path, 'r') as file:
+            lines = file.readlines()[1:]  
+        activities = [line.strip().split('\t') for line in lines]
+        return activities
+
+    def read_trajectory_files(self, trajectory_path):
+        files = [os.path.join(trajectory_path, f) for f in os.listdir(trajectory_path) if f.endswith('.plt')]
+        all_points = []
+        for file in files:
+            with open(file, 'r') as f:
+                points = f.readlines()[6:]  
+                all_points.extend([point.strip().split(',') for point in points])
+        return all_points
+
+    def insert_user(self, user_id, has_labels):
+        query = "INSERT INTO User (id, has_labels) VALUES (%s, %s)"
+        self.cursor.execute(query, (user_id, has_labels))
+        self.db_connection.commit()
+
+    def insert_activity(self, user_id, mode, start_time, end_time):
+        query = """INSERT INTO Activity (user_id, transportation_mode, start_date_time, end_date_time) 
+                VALUES (%s, %s, %s, %s)"""
+        self.cursor.execute(query, (user_id, mode, start_time, end_time))
+        self.db_connection.commit()
+        return self.cursor.lastrowid  
+
+    def insert_trackpoint(self, activity_id, lat, lon, altitude, date_days, date_time):
+        query = """INSERT INTO TrackPoint (activity_id, lat, lon, altitude, date_days, date_time) 
+                VALUES (%s, %s, %s, %s, %s, %s)"""
+        self.cursor.execute(query, (activity_id, lat, lon, altitude, date_days, date_time))
         self.db_connection.commit()
 
     def fetch_data(self, table_name):
@@ -47,7 +84,7 @@ class ExampleProgram:
         # Using tabulate to show the table in a nice way
         print("Data from table %s, tabulated:" % table_name)
         print(tabulate(rows, headers=self.cursor.column_names))
-        return rows """
+        return rows
 
     def drop_table(self, table_name):
         print("Dropping table %s..." % table_name)
@@ -64,15 +101,34 @@ def main():
     program = None
     try:
         program = ExampleProgram()
-        program.create_tables()
-        #program.insert_data(table_name="Person")
-        #_ = program.fetch_data(table_name="Person")
-        program.show_tables()
-        program.drop_table(table_name="User")
-        program.drop_table(table_name="Activity")
         program.drop_table(table_name="TrackPoint")
-        # Check that the table is dropped
-        
+        program.drop_table(table_name="Activity")
+        program.drop_table(table_name="User")  
+        program.create_tables()
+        user_id = "020"
+        user_path = os.path.join("dataset", "dataset", "Data", user_id)
+        labels = program.read_labels(user_path)
+        has_labels = len(labels) > 0
+        program.insert_user(user_id, has_labels)
+
+        for label in labels:
+            start_time, end_time, mode = label
+            activity_id = program.insert_activity(user_id, mode, start_time.replace("/", "-"), end_time.replace("/", "-"))
+
+            trajectory_path = os.path.join(user_path, "Trajectory")
+            trackpoints = program.read_trajectory_files(trajectory_path)
+            print("Starting trackpoint insertion...")
+            for point in trackpoints:
+                print("trackpoint: ", point)
+                lat, lon, _, _, date_days, date, time = point
+                datetime_str = f"{date} {time}"
+                program.insert_trackpoint(activity_id, lat, lon, 0, date_days, datetime_str)
+            print("Finished trackpoint insertion.")
+        program.show_tables()
+        program.fetch_data("User") 
+        program.drop_table(table_name="TrackPoint")
+        program.drop_table(table_name="Activity")
+        program.drop_table(table_name="User")    
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
