4b6e6b48d3c5276a5dc542bfec8fa70da4564e56
quarc0o <emmanuelq150@gmail.com>
2023-10-06 01:30:32 +0200

    alternative solution

--- 8.py
+++ 8.py
@@ -26,7 +26,8 @@ class ExampleProgram:
         SELECT TrackPoint.id, TrackPoint.lat, TrackPoint.lon, Activity.user_id, TrackPoint.date_days, TrackPoint.date_time
         FROM TrackPoint
         JOIN Activity ON TrackPoint.activity_id = Activity.id
-        LIMIT 30000
+        ORDER BY TrackPoint.lat
+        LIMIT 5000;
         """
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
@@ -74,27 +75,9 @@ def main():
     try:
         program = ExampleProgram()
         some_trackpoints = program.get_trackpoint()
-        lon, lat, user_id, date_days, date_time = some_trackpoints
-        count = 0
-        track_count = 0
-        
-        for i, (lon1, lat1, user1, time1) in enumerate(zip(lon, lat, user_id, date_time)):
-            track_count += 1
-            for j, (lon2, lat2, user2, time2) in enumerate(zip(lon, lat, user_id, date_time)):
-                # Check if the trackpoints are not from the same user
-                if user1 != user2:
-                    # Check if timestamps are within 30 minutes of each other
-                    time_diff = abs((time1 - time2).total_seconds())
-                    if time_diff <= 1800:  # 30 minutes = 1800 seconds
-                        # Calculate the distance between the two points
-                        km = program.haversine_np(lon1, lat1, lon2, lat2)
-                        # Check if the distance is less than 0.1 km
-                        if km < 0.1:
-                            count += 1
-                            print(f"Trackpoint {i} ({lon1}, {lat1}, User: {user1}, Time: {time1}) is close to "
-                                  f"Trackpoint {j} ({lon2}, {lat2}, User: {user2}, Time: {time2}) with distance {km:.5f} km")
-        
-        print("Finished comparing", track_count, "trackpoints. Found", count, "trackpoint pairs closer than 0.1 km, within 30 minutes, and not from the same user.")
+        for trackpoint in some_trackpoints:
+            print(trackpoint)
+            print("\n")
         
     except Exception as e:
         print("ERROR: Failed to use database:", e)
